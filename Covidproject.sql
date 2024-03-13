531cd7813d2e102cb273dfe242b23913717df329
Ememobong Umoessien <79858169+Emem-Dave@users.noreply.github.com>
2024-03-13 16:52:08 +0100

    Add files via upload

---
+++ Covidproject.sql
@@ -0,0 +1,96 @@
+select * from CovidDeaths$
+Where continent is not null 
+order by 3,4
+
+--select * from CovidVaccinations$
+--order by 3,4
+
+Select Location, date, total_cases, new_cases, total_deaths, population
+From CovidDeaths$
+Where continent is not null 
+
+
+-- Total Cases vs Total Deaths in Nigeria
+
+Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From CovidDeaths$
+where location like '%Nigeria%'
+and continent is not null 
+order by 1,2
+
+
+
+-- Total Cases vs Population in Nigeria
+-- Shows what percentage of population infected with Covid
+
+
+Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
+From CovidDeaths$
+where location like '%Nigeria%'
+and continent is not null 
+order by 1,2
+
+
+
+-- Countries with Highest Infection Rate compared to Population
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeaths$
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- Countries with Highest Death Count per Population
+
+Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From CovidDeaths$
+Where continent is not null 
+Group by continent
+order by TotalDeathCount desc
+
+
+
+-- BREAKING THINGS DOWN BY CONTINENT
+
+-- Showing contintents with the highest death count per population
+
+Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From CovidDeaths$
+--Where location like '%states%'
+Where continent is not null 
+Group by continent
+order by TotalDeathCount desc
+
+
+-- GLOBAL NUMBERS
+
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From CovidDeaths$
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
+
+select dea.continent, dea.location, dea.date, vac.new_vaccinations,
+SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+from CovidDeaths$ dea
+join CovidVaccinations$ vac
+on dea.location = vac.location
+and dea.date = vac.date
+where dea.continent is not null 
+order by 2,3
+
+
+-- Creating View to store data for later visualizations
+
+Create View PercentPopulationVaccinated as
+select dea.continent, dea.location, dea.date, vac.new_vaccinations,
+SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+from CovidDeaths$ dea
+join CovidVaccinations$ vac
+on dea.location = vac.location
+and dea.date = vac.date
+where dea.continent is not null 
